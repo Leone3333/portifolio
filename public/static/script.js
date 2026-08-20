31eae7b2c9ca888ef8f93aaae7a7534fa0519ea7
leone3333 <leonegoveia1@gmail.com>
2026-08-20 15:59:46 -0300

    side bar configurada para mobile

--- public/static/script.js
+++ public/static/script.js
@@ -1,6 +1,8 @@
 let btn_menu = document.querySelector('#menuBtn');
 let side_bar = document.querySelector('.sideBarDiv');
-const textos_menu = document.querySelectorAll('.nav_item span');
+let textos_menu = document.querySelectorAll('.nav_item span');
+let btn_mobile = document.getElementById('btnMobile');
+
 
 btn_menu.addEventListener('click', () => {
     console.log(textos_menu);
@@ -15,6 +17,28 @@ btn_menu.addEventListener('click', () => {
         });
         
     } else {
+        btn_mobile.classList.remove('d-none');
+        btn_menu.classList.replace('bi-x-lg', 'bi-list');
+        textos_menu.forEach((span) => {
+            span.classList.toggle('d-none');
+        });
+    }
+})
+    btn_mobile.addEventListener('click', () => {
+    console.log(textos_menu);
+    side_bar.classList.toggle('open');
+
+    if (side_bar.classList.contains('open')) {
+        btn_mobile.classList.add('d-none');
+        btn_menu.classList.replace('bi-list', 'bi-x-lg');
+
+        textos_menu.forEach((span) => {
+            // Exemplo: removendo a classe d-none para mostrar o texto
+            span.classList.remove('d-none');
+        });
+        
+    } else {
+        btn_mobile.classList.remove('d-none');
         btn_menu.classList.replace('bi-x-lg', 'bi-list');
         textos_menu.forEach((span) => {
             // Exemplo: removendo a classe d-none para mostrar o texto
@@ -23,6 +47,3 @@ btn_menu.addEventListener('click', () => {
     }
 })
 
-const sideBar = () => {
-
-}
